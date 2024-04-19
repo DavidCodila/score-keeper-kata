6d6526d4045bc745c52a738e78c59b9e68e6d70d
david <Davids-MacBook-Pro.local>
2024-04-19 14:53:53 +1000

    Passed test scoreTeamA1

--- src/ScoreKeeper.ts
+++ src/ScoreKeeper.ts
@@ -2,7 +2,9 @@ export class ScoreKeeper {
   private teamA: number = 0;
   private teamB: number = 0;
 
-  scoreTeamA1(): void {}
+  scoreTeamA1(): void {
+    this.teamA++;
+  }
   getTeamAScore(): number {
     return this.teamA;
   }
