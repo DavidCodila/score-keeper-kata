771016702495c36b3704c0317f5291859d9d46bb
david <Davids-MacBook-Pro.local>
2024-04-19 15:08:27 +1000

    Added failing test getScore

--- __tests__/ScoreKeeper.test.ts
+++ __tests__/ScoreKeeper.test.ts
@@ -6,3 +6,50 @@ test("test scoreTeamA1", () => {
   sk.scoreTeamA1();
   expect(sk.getTeamAScore()).toEqual(1);
 });
+
+test("test scoreTeamA2", () => {
+  const sk = new ScoreKeeper();
+  sk.scoreTeamA2();
+  expect(sk.getTeamAScore()).toEqual(2);
+});
+
+test("test scoreTeamA3", () => {
+  const sk = new ScoreKeeper();
+  sk.scoreTeamA3();
+  expect(sk.getTeamAScore()).toEqual(3);
+});
+
+test("test scoreTeamB1", () => {
+  const sk = new ScoreKeeper();
+  sk.scoreTeamB1();
+  expect(sk.getTeamBScore()).toEqual(1);
+});
+
+test("test scoreTeamB2", () => {
+  const sk = new ScoreKeeper();
+  sk.scoreTeamB2();
+  expect(sk.getTeamBScore()).toEqual(2);
+});
+
+test("test scoreTeamB3", () => {
+  const sk = new ScoreKeeper();
+  sk.scoreTeamB3();
+  expect(sk.getTeamBScore()).toEqual(3);
+});
+
+test("test getScore", () => {
+  const sk = new ScoreKeeper();
+  sk.scoreTeamA1();
+  sk.scoreTeamA2();
+  sk.scoreTeamA3();
+  sk.scoreTeamA3();
+  sk.scoreTeamA2();
+  sk.scoreTeamA2();
+  sk.scoreTeamB3();
+  sk.scoreTeamB3();
+  sk.scoreTeamB3();
+  sk.scoreTeamB2();
+  sk.scoreTeamB3();
+  sk.scoreTeamB1();
+  expect(sk.getScore()).toEqual("013:015");
+});
--- src/ScoreKeeper.ts
+++ src/ScoreKeeper.ts
@@ -3,9 +3,33 @@ export class ScoreKeeper {
   private teamB: number = 0;
 
   scoreTeamA1(): void {
-    this.teamA++;
+    this.teamA += 1;
   }
+  scoreTeamA2(): void {
+    this.teamA += 2;
+  }
+  scoreTeamA3(): void {
+    this.teamA += 3;
+  }
+
   getTeamAScore(): number {
     return this.teamA;
+  }
+
+  scoreTeamB1(): void {
+    this.teamB += 1;
+  }
+  scoreTeamB2(): void {
+    this.teamB += 2;
+  }
+  scoreTeamB3(): void {
+    this.teamB += 3;
+  }
+
+  getTeamBScore(): number {
+    return this.teamB;
+  }
+  getScore(): string {
+    return "";
   }
 }
